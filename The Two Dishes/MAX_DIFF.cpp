e34ee948ff1449b19fbec31e733d6d04e6afe6a0
Rahul <86495244+RahulBRB@users.noreply.github.com>
2022-10-20 12:10:11 +0530

    Create MAX_DIFF.cpp

---
+++ The Two Dishes/MAX_DIFF.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    
+	    int n,s;
+	    cin>>n>>s;
+	    
+	    if(s<=n){
+	        cout<<s<<endl;
+	    }
+	    else{
+	        cout<<(n-s)+n<<endl;
+	    }
+	    
+	    
+	}
+	return 0;
+}
